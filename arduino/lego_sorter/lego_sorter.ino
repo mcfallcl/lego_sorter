5b6c43a83828e9dd65f790e58b6ce922ce2c226e
Christopher McFall <mcfallcl@gmail.com>
2019-01-23 13:22:50 -0500

    Implement i2c in .ino file
    Signed-off-by: Christopher McFall <mcfallcl@gmail.com>

--- arduino/lego_sorter/lego_sorter.ino
+++ arduino/lego_sorter/lego_sorter.ino
@@ -1,19 +1,53 @@
-#include "Motor.hpp"
+#include <Wire.h>
+
 #include "MotorController.hpp"
+#include "Conveyor.hpp"
+#include "Hopper.hpp"
+
+const byte i2c_slave_addr = 0x55;
+uint8_t i2c_reg_addr = 0;
+uint8_t i2c_response = 0;
 
 MotorController<3> mc;
 
+Conveyor conveyor1(mc.createMotor(9, 10, 11));
+Conveyor conveyor2(mc.createMotor(12, 13, 14));
+Hopper hopper(mc.createMotor(15, 16, 17));
+
 void setup()
 {
-  //Serial.begin(9600);
+    //Serial.begin(9600);
 
-  Motor &m1 = mc.createMotor(9, 10, 11);
-
-  m1.set_cycle_period(500);
-  m1.enable();
+    Wire.begin(i2c_slave_addr);
+    Wire.setClock(50000L);
+    Wire.onReceive(i2c_recv_int);
+    Wire.onRequest(i2c_respond);
 }
 
 void loop()
 {
-  mc.cycle();
+    mc.cycle();
+}
+
+void i2c_recv_int(int x)
+{
+    i2c_reg_addr = Wire.read();
+    // Do things based on addr
+    // make sure to set i2c_response
+}
+
+void i2c_respond()
+{
+    Wire.write(i2c_response);
+}
+
+namespace
+{
+    // i2c responses
+    const uint8_t NACK = 0x00;
+}
+
+uint8_t handle_i2c(uint8_t in)
+{
+    uint8_t out = NACK;
 }
